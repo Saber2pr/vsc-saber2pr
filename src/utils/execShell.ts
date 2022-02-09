ef78a2d96d19c4b238c9206b4cc7fabc6c9b944b
muhai <muhai.li@tuya.com>
2022-02-09 15:27:27 +0800

    fix: execShell

--- src/utils/execShell.ts
+++ src/utils/execShell.ts
@@ -3,12 +3,30 @@ import { spawn } from 'child_process'
 import { getRootPath } from './editor'
 
 export const execShell = (command: string, args: string[]) =>
-  new Promise<string>(resolve => {
+  new Promise<string>((resolve, reject) => {
     const task = spawn(command, args, {
       cwd: getRootPath()?.fsPath,
       env: process.env,
       shell: true,
       stdio: 'inherit',
     })
-    task.on('close', resolve)
+
+    let result = ''
+    let error = ''
+    task.stdout.on('data', data => {
+      result += data
+    })
+
+    task.stderr.on('data', data => {
+      error += data
+    })
+
+    task.on('close', () => {
+      if (result) {
+        resolve(result)
+      }
+      if (error) {
+        reject(error)
+      }
+    })
   })
